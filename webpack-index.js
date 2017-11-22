5249b6d595494e13c43a3257c64a50141dae0a8e
Simon Bobrov <simon@trdhub.com>
2017-11-22 15:36:27 +0100

    added webpack support

---
+++ webpack-index.js
@@ -0,0 +1,9 @@
+import * as comparison from './lib/comparison';
+import * as date from './lib/date';
+import * as math from './lib/math';
+import * as number from './lib/number';
+import * as object from './lib/object';
+import * as string from './lib/string';
+import * as url from './lib/url';
+
+module.exports = Object.assign({}, comparison, date, math, number, object, string, url);
